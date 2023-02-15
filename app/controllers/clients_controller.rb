6484bcff67632f929030cd776b031c792f665215
Philip <fili.cueavas@gmail.com>
2023-02-14 21:13:03 -0600

    Create Client controller CRUD, routes for clients and views

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,49 @@
+class ClientsController < ApplicationController
+  before_action :setup_client, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @clients = Client.all
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+
+    if @client.save
+      redirect_to @client
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @client.update(client_params)
+      redirect_to @client
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @client.destroy
+    redirect_to clients_url, status: :see_other
+  end
+
+  private
+    def client_params
+      params.require(:client).permit(:first_name, :last_name, :email, :phone)
+    end
+
+    def setup_client
+      @client = Client.find(params[:id])
+    end
+end

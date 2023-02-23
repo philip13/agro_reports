0e9ffbf77be19f1f53eef6b6b8c32d88b965546c
Philip <fili.cueavas@gmail.com>
2023-02-22 22:26:02 -0600

    New seed for clients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Clear any existing clients
+Client.destroy_all
+
+# Create new client records using Faker gem
+10.times do
+  Client.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.cell_phone
+  )
+end

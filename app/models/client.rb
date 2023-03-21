5594f4ef0051a68d4693e80439f0d9b014226ed8
Philip <fili.cueavas@gmail.com>
2023-03-20 20:46:50 -0600

    Include new method to get full_name

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,8 @@
 class Client < ApplicationRecord
   validates :first_name, :last_name, :phone, presence: true
   validates :email, presence: true, email: true
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end

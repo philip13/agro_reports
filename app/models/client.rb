62b062fbc7ebcfa934d24c770b9c5293ec0cfcd6
Philip <fili.cueavas@gmail.com>
2023-03-20 20:52:36 -0600

    Fix 'self' to get paramether value is offenses on rails models

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,6 @@ class Client < ApplicationRecord
   validates :email, presence: true, email: true
 
   def full_name
-    "#{self.first_name} #{self.last_name}"
+    "#{first_name} #{last_name}"
   end
 end

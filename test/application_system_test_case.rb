ddde21c0550118809f5a971cc6f78f940ce37fa1
Philip <fili.cueavas@gmail.com>
2023-03-24 14:37:50 -0600

    Fix offenses indentation

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-    DRIVER = if ENV["DRIVER"]
-      ENV["DRIVER"].to_sym
-    else
-      :headless_chrome
-    end
+  DRIVER = if ENV["DRIVER"]
+    ENV["DRIVER"].to_sym
+  else
+    :headless_chrome
+  end
 
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 end

55aa4ce6a9fb0b2061a33016923ca6b03d8ec58a
Philip <fili.cueavas@gmail.com>
2023-03-31 21:03:02 -0600

    remove helper model buider with_account on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   has_one :account, foreign_key: "owner_id", inverse_of: :owner
   accepts_nested_attributes_for :account
 
-  def with_account
-    build_account if account.nil?
-    self
-  end
+  # def with_account
+  #   build_account if account.nil?
+  #   self
+  # end
 end

a78c4dd1cb0007532c0a5bcc599118f5cbe0d853
Philip <fili.cueavas@gmail.com>
2023-03-31 21:03:47 -0600

    Include new syste test to login

---
+++ test/system/user_login_test.rb
@@ -0,0 +1,18 @@
+require "application_system_test_case"
+
+class UserLoginTest < ApplicationSystemTestCase
+  test "Login" do
+    user = users(:user1)
+    visit new_user_session_path
+
+    assert_selector "h2", text: "Login"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "123456"
+    click_button "Login"
+
+    assert_text "Signed in successfully."
+    assert_selector "h2", text: "Account Details"
+    assert_text "Account1"
+    assert_text user.email
+  end
+end

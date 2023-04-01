bd6434a224e5bdbdcca669d2746280f69e9f72f9
Philip <fili.cueavas@gmail.com>
2023-03-31 20:31:38 -0600

    fix a offenses

--- db/migrate/20230328171132_add_owner_id_from_account.rb
+++ db/migrate/20230328171132_add_owner_id_from_account.rb
@@ -1,5 +1,5 @@
 class AddOwnerIdFromAccount < ActiveRecord::Migration[7.0]
   def change
-    add_reference :accounts, :owner, foreign_key: { to_table: :users }, null: false
+    add_reference :accounts, :owner, foreign_key: {to_table: :users}, null: false
   end
 end

891a4fffcce0f278b28d796b0b615d41a242b8d5
Philip <fili.cueavas@gmail.com>
2023-04-01 11:03:44 -0600

    remove helper builder method to create account

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,4 @@ class User < ApplicationRecord
 
   has_one :account, foreign_key: "owner_id", inverse_of: :owner
   accepts_nested_attributes_for :account
-
-  # def with_account
-  #   build_account if account.nil?
-  #   self
-  # end
 end

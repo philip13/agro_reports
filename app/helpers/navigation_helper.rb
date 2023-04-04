dcc4a19f3655a08ababc4385fcd1651a026141af
Philip <fili.cueavas@gmail.com>
2023-04-04 13:15:16 -0600

    Update sidebar app name and navigation links

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,5 +1,5 @@
 module NavigationHelper
-  def link_class link_path
+  def link_class(link_path)
     if current_page?(link_path)
       "a-active a-exact-active flex items-center px-6 py-2 mt-4 duration-200 border-l-4 bg-gray-600 bg-opacity-25 text-gray-100 border-gray-100"
     else
